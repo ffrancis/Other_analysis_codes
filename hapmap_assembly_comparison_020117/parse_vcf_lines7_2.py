66a507d2c879397da43895e276b6abce5fde17f1
ffrancis <felixfrancier@gmail.com>
2017-02-01 15:20:17 -0500

    separated coordinate and genotype subsetting

--- hapmap_assembly_comparison_020117/parse_vcf_lines7_2.py
+++ hapmap_assembly_comparison_020117/parse_vcf_lines7_2.py
@@ -23,32 +23,26 @@ t0 = time.time()
 ### INPUTS
 ############################################################
 
-# input_path   =   "/mnt/data27/ffrancis/HapMap3/all_lines/"
-input_path   =   "/mnt/data27/ffrancis/HapMap3/HapMap3_01302017/un_imputed_V4coords/"
+# input_path   =   "/mnt/data27/ffrancis/HapMap3/all_lines/"                                ### first release HapMap3 vcf files V3 coords
+input_path   =   "/mnt/data27/ffrancis/HapMap3/HapMap3_01302017/un_imputed_V4coords/"       ### latest HapMap3 vcf files V4 coords
 
-
-# select_line     = "Tx303"
+### line name
 selected_line     = '282set_Tx303'
 
-### actual data
-# input_file   =   "c1_hmp31_q30.vcf"
-# input_file   =   input_path + "hmp321_agpv4_chr1.vcf"
-input_file   =  "test_hmp321_agpv4_chr1.vcf"
-
-### V3 coords
-# locus_start = 25375814
-# locus_stop = 25398735
-
+### input vcf file
+input_file   =    "hmp321_agpv4_chr1.vcf"
 
 ### V4 coords qNLB_1_25722269_22589
-
-# locus_start = 25722269 
-# locus_stop = 25744857
+locus_start = 25722269 
+locus_stop = 25744857
 
 ### test data
-# input_file   =   "c10_first1000.vcf"
-locus_start =  56032
-locus_stop = 56140
+# input_file   =  "test_hmp321_agpv4_chr1.vcf"
+# locus_start =  56032
+# locus_stop = 57140
+
+
+output_file_name = "coord_subsetted_"
 
 
 ############################################################
@@ -68,28 +62,44 @@ def get_line_column_no(selected_line, N_lines):
         return int(column_pos)
 
 
-def subset_hapmap(input_path, input_file,output_file_name):
+def coord_subset_hapmap(input_path, input_file,output_file_name):
     selected_line_column = get_line_column_no(selected_line, 30)
     df = pd.read_csv(input_path +input_file, sep='\t', comment='#', skiprows=0, usecols=[1, 3, 4, int(selected_line_column)], header=None)
     df = df[(df[1] >= int(locus_start)) & (df[1] <= int(locus_stop))]
-    ### split column by delimiter
-    df_column_split = pd.DataFrame(df[selected_line_column].str.split(':',1).tolist(), columns = ['genotype','extra'])
-    ### merged the split df with the original df
-    df = df.join(df_column_split)
-    ### remove redundant columns
-    df = df.drop(selected_line_column, 1)
-    df = df.drop('extra', 1)
-    ### remove columns with no variants called / missing data
-    df = df.loc[df['genotype'] != '0/0']
-    df = df.loc[df['genotype'] != './.']
     df.columns = ['Coordinate', 'REF_allele','ALT_allele', "Genotype"]
     df.to_csv(output_file_name + input_file[:-4] + ".txt", sep='\t', encoding='utf-8', index=False)
+
+def variant_subset_hapmap(output_file_name, input_file):
+    df2 = pd.read_csv(output_file_name + input_file[:-4] + ".txt", sep='\t')
+    ### split column by delimiter
+    df_column_split = pd.DataFrame(df2.Genotype.str.split(':',1).tolist(), columns = ['genotype','extra'])
+    ### merged the split df with the original df
+    df2 = df2.join(df_column_split)
+    ### remove columns with no variants called / missing data
+    df2 = df2.loc[df2['genotype'] != '0/0']
+    df2 = df2.loc[df2['genotype'] != './.']
+    df2 = df2.drop('Genotype', 1)
+    df2 = df2.drop('extra', 1)
+    df2.to_csv('variant_subsetted_' + input_file[:-4] + '.txt', sep='\t', encoding='utf-8', index=False)
     
 ############################################################
 ### CODE
 ############################################################
         
 if __name__ =='__main__':
-    subset_hapmap(input_path, input_file,"subsetted_")
 
+
+    ### subset hapmap by coordinates
+    # coord_subset_hapmap(input_path, input_file,output_file_name)
+
+    
+    ### filter based on selected line genotype
+    variant_subset_hapmap(output_file_name, input_file)
+
+
+
+    
+    
+    
+    
 
