850ba2bead251dd16fa3290d8bd9eebbaddcb60a
ffrancis <felixfrancier@gmail.com>
2017-02-01 19:29:19 -0500

    functions

--- hapmap_assembly_comparison_020117/global_alignment_variants.py
+++ hapmap_assembly_comparison_020117/global_alignment_variants.py
@@ -31,6 +31,21 @@ t0 = time.time()
 
 
 ############################################################
+### INPUT FILES
+############################################################
+
+input_file   =   "aln_Tx303_B73v322kb.txt"
+input_file   =   "aln_Tx303_B73v322kb_test.txt"
+format = "emboss"
+# format = "pair"
+
+
+seq_1 = "./sequences/TA_2016-10-11T10_46_30_113201_1_25376615_22184_PolyMasked.fasta"
+seq_2 = "./sequences/Tx303HPLC_BARDCODE_f_2_Tx303HPLC_BARDCODE_r_2merged_reads_assembly.fasta"
+
+same_strand = 0  # (0: not same strand; 1: same strand)
+
+############################################################
 ### FUNCTIONS
 ############################################################
 ### get rev complement of a sequence
@@ -54,72 +69,38 @@ def get_fasta_seq(input_fasta):
         header, sequence = record.id, str(record.seq)
         return sequence
 
-############################################################
-### INPUT FILES
-############################################################
-
-input_file   =   "aln_Tx303_B73v322kb.txt"
-input_file   =   "aln_Tx303_B73v322kb_test.txt"
-format = "emboss"
-# format = "pair"
-
-
-seq_1 = "./sequences/TA_2016-10-11T10_46_30_113201_1_25376615_22184_PolyMasked.fasta"
-seq_2 = "./sequences/Tx303HPLC_BARDCODE_f_2_Tx303HPLC_BARDCODE_r_2merged_reads_assembly.fasta"
-
-
-
-
-
-
+def pairwise_global_alignment(seq_1, seq_2):
+    ### process input sequences
+    seq_1 = get_fasta_seq(seq_1)
+    seq_2 = get_fasta_seq(seq_2)
+    if same_strand == 0:
+        seq_1 = rev_complement(seq_1)
+    seq_1 = "asis:" + seq_1
+    seq_2 = "asis:" + seq_2
+    ### needle alignment
+    needle_cline = NeedleCommandline(asequence=seq_1, bsequence=seq_2, gapopen=16, gapextend=4, outfile="needle.txt")
+    stdout, stderr = needle_cline()
 
 ############################################################
 ### CODE
 ############################################################
 
 
-seq_1 = get_fasta_seq(seq_1)
-seq_2 = get_fasta_seq(seq_2)
-seq_1 = rev_complement(seq_1)
+if __name__ =='__main__':
+    # pairwise_global_alignment(seq_1, seq_2)
 
+    
+    
+    
+    ### read alignment file
+    align = AlignIO.read("needle.txt", "emboss")
+    # print(align)
 
-seq_1 = "asis:" + seq_1
-seq_2 = "asis:" + seq_2
+    alignment = AlignIO.read(open("needle.txt"), "emboss")
+    # for record in alignment :
+        # print(record.seq + " " + record.id)
 
-####
-# for a in pairwise2.align.globalxx(seq_1, seq_2):
-    # print(format_alignment(*a))
-####
-
-'''
-### stretcher alignment
-from Bio.Emboss.Applications import StretcherCommandline
-stretcher_cline = StretcherCommandline(asequence=seq_1, bsequence=seq_2, gapopen=16, gapextend=4, outfile="stretcher.txt")
-# print stretcher_cline
-stdout, stderr = stretcher_cline()
-# print(stdout + stderr)
-
-### read alignment file
-# align = AlignIO.read("stretcher.txt", "emboss")
-align = AlignIO.read("stretcher.txt", "markx0")
-print(align)
-'''
-
-
-### needle alignment
-
-# needle_cline = NeedleCommandline(asequence=seq_1, bsequence=seq_2, gapopen=16, gapextend=4, outfile="needle.txt")
-# stdout, stderr = needle_cline()
-
-### read alignment file
-# align = AlignIO.read("needle.txt", "emboss")
-# print(align)
-
-alignment = AlignIO.read(open("needle.txt"), "emboss")
-# for record in alignment :
-	# print(record.seq + " " + record.id)
-
-print alignment[0].seq
+    print alignment[0].seq
 
 
 
