088edf874f5d9ccc9989042f35eed118fc5f950f
ffrancis <felixfrancier@gmail.com>
2017-02-10 09:30:46 -0500

    variants from two or more sequence assemblies

--- hapmap_assembly_comparison_020117/global_alignment_variants.py
+++ hapmap_assembly_comparison_020117/global_alignment_variants.py
@@ -1,9 +1,9 @@
-### Get variants from global alignment of two sequences
-### Version 1.0.0: 02/01/2017
+### Get variants from multiple sequences
+###	a pairwise sequence alignment is carried out, from which the variants are recorded as a vcf file
+### Version 1.0.0: 02/09/2017
 ### Author: Felix Francis (felixfrancier@gmail.com)
 
-### Requirements
-###
+
 
 ############################################################
 ### Time to run the code: start timer
@@ -15,12 +15,15 @@ t0 = time.time()
 import datetime
 import os
 import pandas as pd
+import subprocess as sp
 import numpy as np
+import glob
 from Bio import SeqIO
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio import AlignIO                                 ### to read external alignment files
-from Bio.Emboss.Applications import NeedleCommandline   ### for Emboss Needle alignment
+# from Bio.Emboss.Applications import NeedleCommandline   ### for Emboss Needle alignment
+from Bio.Align.Applications import ClustalOmegaCommandline ### for Emboss ClustalOmega
 
 ############################################################
 ### Time to run the code: start timer
@@ -31,19 +34,26 @@ t0 = time.time()
 
 
 ############################################################
-### INPUT FILES
+### INPUT FILES & PATHS
 ############################################################
 
-input_file   =   "aln_Tx303_B73v322kb.txt"
-input_file   =   "aln_Tx303_B73v322kb_test.txt"
+msa2vcf_path	= "/home/ffrancis/apps/jvarkit/dist/msa2vcf.jar"
+sequence_path	= "../sequences/"
+reference_path	= "../sequences/B73_reference/"
+
 format = "emboss"
-# format = "pair"
 
 
-seq_1 = "./sequences/TA_2016-10-11T10_46_30_113201_1_25376615_22184_PolyMasked.fasta"
-seq_2 = "./sequences/Tx303HPLC_BARDCODE_f_2_Tx303HPLC_BARDCODE_r_2merged_reads_assembly.fasta"
+
+# reference = "./sequences/TA_2016-10-11T10_46_30_113201_1_25376615_22184_PolyMasked.fasta"		### v3 locus REFERENCE
+# reference = "../sequences/v4_locus.fasta"														### v4_locus REFERENCE
+reference	= "Chr1_25375814_25398735.fasta"													### v3_locus REFERENCE
+query		= "Tx303HPLC.fasta"
 
 same_strand = 0  # (0: not same strand; 1: same strand)
+
+
+
 
 ############################################################
 ### FUNCTIONS
@@ -69,41 +79,67 @@ def get_fasta_seq(input_fasta):
         header, sequence = record.id, str(record.seq)
         return sequence
 
-def pairwise_global_alignment(seq_1, seq_2):
+def pairwise_global_alignment(reference, query):
     ### process input sequences
-    seq_1 = get_fasta_seq(seq_1)
-    seq_2 = get_fasta_seq(seq_2)
+    reference = get_fasta_seq(reference)
+    query = get_fasta_seq(query)
     if same_strand == 0:
-        seq_1 = rev_complement(seq_1)
-    seq_1 = "asis:" + seq_1
-    seq_2 = "asis:" + seq_2
+        query = rev_complement(query)
+    reference = "asis:" + reference
+    query = "asis:" + query
     ### needle alignment
-    needle_cline = NeedleCommandline(asequence=seq_1, bsequence=seq_2, gapopen=16, gapextend=4, outfile="needle.txt")
+    needle_cline = NeedleCommandline(asequence=reference, bsequence=query, gapopen=16, gapextend=4, outfile="needle.txt")
     stdout, stderr = needle_cline()
 
+def clustal_omega_alignment(in_file, out_file):
+    ### process input sequences
+    # in_file = "asis:" + in_file
+    ### needle alignment
+    clustalomega_cline = ClustalOmegaCommandline(infile=in_file, outfile=out_file, verbose=True, auto=True) 
+    stdout, stderr = clustalomega_cline()
+
+	
+def read_fasta(input_fasta):
+	handle	= open(input_fasta, "rU")
+	for record in SeqIO.parse(handle, "fasta") :
+		header, sequence = record.id, str(record.seq)
+		return [header, sequence]
+		
 ############################################################
 ### CODE
 ############################################################
-
-
 if __name__ =='__main__':
-    # pairwise_global_alignment(seq_1, seq_2)
+	### merge input sequences
 
-    
-    
-    
-    ### read alignment file
-    align = AlignIO.read("needle.txt", "emboss")
-    # print(align)
+	### get all fasta files in sequences diretory
+	file_list = glob.glob(str(sequence_path) + "*.fasta")
+	for file in file_list:
+		query = str(file.split('/')[-1])
+		print query
+	
+		# with open('./merged.fasta', 'w') as outfile:
+		with open("./" + str(query[:-6]) + "_merged.fasta", 'w') as outfile:
+			r_header_sequence = read_fasta(reference_path + reference)
+			r_header, r_sequence = r_header_sequence[0], r_header_sequence[1]
+			outfile.write(">"+ r_header + '\n')
+			outfile.write(r_sequence + '\n')
+			q_header_sequence = read_fasta(sequence_path + query)
+			q_header, q_sequence = q_header_sequence[0], q_header_sequence[1]
+			q_sequence = rev_complement(q_sequence)
+			outfile.write(">"+ q_header + '\n')
+			outfile.write(q_sequence + '\n')
 
-    alignment = AlignIO.read(open("needle.txt"), "emboss")
-    # for record in alignment :
-        # print(record.seq + " " + record.id)
-
-    print alignment[0].seq
+		# clustal_omega_alignment("./" + str(query[:-6]) + "_merged.fasta", "clustalo_output")
+		clustal_omega_alignment("./" + str(query[:-6]) + "_merged.fasta", str(query[:-6])+ "_alignment_output")
 
 
-
+		f0 = open(os.devnull, 'w')
+		with open(str(reference[:-6]) + "_" + str(query[:-6]) + ".vcf","wb") as out:
+			# sp.Popen(["java", "-jar", "%s" %msa2vcf_path,"./clustalo_output"],stdout=out, stderr=f0)
+			sp.Popen(["java", "-jar", "%s" %msa2vcf_path,"./"+str(query[:-6])+ "_alignment_output"],stdout=out, stderr=f0)
+		
+		
+		
 ############################################################
 #Time to run the code: end timer
 ############################################################
