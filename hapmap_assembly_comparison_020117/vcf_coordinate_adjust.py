819ac6e25e9950db8140658533d85fb26688cfcc
ffrancis <felixfrancier@gmail.com>
2017-02-14 12:02:00 -0500

    coordinate adjust; reference based

--- hapmap_assembly_comparison_020117/vcf_coordinate_adjust.py
+++ hapmap_assembly_comparison_020117/vcf_coordinate_adjust.py
@@ -74,8 +74,12 @@ def adjust_vcf_coordinates(input_path, input_file, output_file, reference):
 						adj_REF, adj_ALT = ALT, REF
 					else:
 						adj_REF, adj_ALT = REF, ALT
+					if len(adj_ALT) > len(adj_REF):
+						adj_ALT = '<INS>'
+					elif len(adj_REF) > len(adj_ALT):
+						adj_ALT = '<DEL>'
 					### adjust coordiate by accounting for reference gaps in alignment for previous genotype pos
-					POS = int(POS) - coord_adj
+					POS = (int(POS) - coord_adj) + reference_start
 					output.write(str(CHROM)+'\t'+str(POS)+'\t'+str(ID)+'\t'+str(adj_REF)+'\t'+str(adj_ALT)+'\t'+str(QUAL)+'\t'+str(FILTER)+'\t'+str(INFO)+'\t'+str(FORMAT)+'\t'+ '0/0:1' +'\t'+'1/1:1'+ '\n')
 					### adjust coordiate by accounting for reference gaps in alignment for next genotype pos
 					if len(adj_ALT) > len(adj_REF):
