13f29c551fcff57f0be8ac15a5d1b9c005aca839
ffrancis <felixfrancier@gmail.com>
2017-02-15 11:27:08 -0500

    coor adj based on ref alt allele len difference

--- hapmap_assembly_comparison_020117/vcf_coordinate_adjust.py
+++ hapmap_assembly_comparison_020117/vcf_coordinate_adjust.py
@@ -29,8 +29,13 @@ reference	= 'CHR1:25375814..25398735'
 
 ### input vcf file
 input_file		=	"Chr1_25375814_25398735_Tx303HPLC.vcf"					### msa2vcf output file
+# input_file		=	"Chr1_25375814_25398735_CML277_HPLC.vcf"					### msa2vcf output file
+# input_file		=	"Chr1_25375814_25398735_Mo17_HPLC.vcf"					### msa2vcf output file
+# input_file		=	"Chr1_25375814_25398735_Hp301_HPLC.vcf"					### msa2vcf output file
+# input_file		=	"Chr1_25375814_25398735_P39_HPLC.vcf"					### msa2vcf output file
 input_path		=	"./"
-output_file		=	"coord_adj_Chr1_25375814_25398735_Tx303HPLC.vcf"
+# output_file		=	"coord_adj_Chr1_25375814_25398735_Tx303HPLC.vcf"
+output_file		=	"coord_adj_" + input_file
 
 reference_start = 25375814		### resequenced extended coordinates
 reference_stop = 25398735		### resequenced extended coordinates
@@ -74,16 +79,24 @@ def adjust_vcf_coordinates(input_path, input_file, output_file, reference):
 						adj_REF, adj_ALT = ALT, REF
 					else:
 						adj_REF, adj_ALT = REF, ALT
-					if len(adj_ALT) > len(adj_REF):
-						adj_ALT = '<INS>'
-					elif len(adj_REF) > len(adj_ALT):
-						adj_ALT = '<DEL>'
+					# if len(adj_ALT) > len(adj_REF):
+						# adj_ALT = '<INS>'
+					# elif len(adj_REF) > len(adj_ALT):
+						# adj_ALT = '<DEL>'
+						
 					### adjust coordiate by accounting for reference gaps in alignment for previous genotype pos
 					POS = (int(POS) - coord_adj) + reference_start
-					output.write(str(CHROM)+'\t'+str(POS)+'\t'+str(ID)+'\t'+str(adj_REF)+'\t'+str(adj_ALT)+'\t'+str(QUAL)+'\t'+str(FILTER)+'\t'+str(INFO)+'\t'+str(FORMAT)+'\t'+ '0/0:1' +'\t'+'1/1:1'+ '\n')
+					
+					if len(adj_ALT) > len(adj_REF):
+						output.write(str(CHROM)+'\t'+str(POS)+'\t'+str(ID)+'\t'+str(adj_REF)+'\t'+'<INS>'+'\t'+str(QUAL)+'\t'+str(FILTER)+'\t'+str(INFO)+'\t'+str(FORMAT)+'\t'+ '0/0:1' +'\t'+'1/1:1'+ '\n')
+					elif len(adj_REF) > len(adj_ALT):
+						output.write(str(CHROM)+'\t'+str(POS)+'\t'+str(ID)+'\t'+str(adj_REF)+'\t'+'<DEL>'+'\t'+str(QUAL)+'\t'+str(FILTER)+'\t'+str(INFO)+'\t'+str(FORMAT)+'\t'+ '0/0:1' +'\t'+'1/1:1'+ '\n')
+					else:
+						output.write(str(CHROM)+'\t'+str(POS)+'\t'+str(ID)+'\t'+str(adj_REF)+'\t'+str(adj_ALT)+'\t'+str(QUAL)+'\t'+str(FILTER)+'\t'+str(INFO)+'\t'+str(FORMAT)+'\t'+ '0/0:1' +'\t'+'1/1:1'+ '\n')
 					### adjust coordiate by accounting for reference gaps in alignment for next genotype pos
 					if len(adj_ALT) > len(adj_REF):
-						coord_adj += len(adj_ALT)-1
+						# coord_adj += len(adj_ALT) - 1
+						coord_adj += len(adj_ALT) - len(adj_REF)
     
 ############################################################
 ### CODE
